463d96c891f9a96be8396348c617e3f72f6c975b
Kenny Lei <klei@jpl.nasa.gov>
2014-04-11 16:13:37 -0400

    Server post stuff

--- node_server/app.js
+++ node_server/app.js
@@ -12,18 +12,20 @@ http.createServer(function (request, response) {
     });
     request.on('end', function() {
       var formData = qs.parse(requestBody);
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write('<!doctype html><html><head><title>response</title></head><body>');
-      response.write('Thanks for the data!<br />User Name: '+formData.UserName);
-      response.write('<br />Repository Name: '+formData.Repository);
-      response.write('<br />Branch: '+formData.Branch);
+      //response.writeHead(200, {'Content-Type': 'text/html'});
+      //response.write('<!doctype html><html><head><title>response</title></head><body>');
+      //response.write('Thanks for the data!<br />User Name: '+formData.UserName);
+      //response.write('<br />Repository Name: '+formData.Repository);
+      //response.write('<br />Branch: '+formData.Branch);
 
       //send data to universal database
 
       //wait
 
       //check database for match
-      response.end('</body></html>');
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end('got it')
+      //response.end('</body></html>');
     });
 }).listen(1337);
 console.log('Server running at http://127.0.0.1:1337/');
--- wearable_gestures/src/js/pebble-js-app.js
+++ wearable_gestures/src/js/pebble-js-app.js
@@ -7,13 +7,35 @@ Pebble.addEventListener("ready",
 Pebble.addEventListener("appmessage",
                         function(e) {
 
-var output;
-for (var i=0;i < 25;i++)
-{
-var onemeasure = e.payload[i] + "\n";
-output += onemeasure;
-}              
-console.log(output);
+	var output;
+	for (var i=0;i < 25;i++)
+	{
+		var onemeasure = e.payload[i] + "\n";
+		output += onemeasure;
+	}              
+	console.log(output);
+
+	var location;
+	navigator.geolocation.getCurrentPosition(function(position){
+		location = position.coords;
+	});
+	var d = new Date();
+
+	var http = new XMLHttpRequest();
+	var url = "http://140.247.0.19:1337/";
+	var params = "latitude="+location.latitude+"&longitude="+location.longitude+"&time="+d.getTime();
+	http.open('POST', url, true);
+	//Send the proper header information along with the request
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.setRequestHeader("Content-length", params.length);
+	http.setRequestHeader("Connection", "close");
+
+	http.onreadystatechange = function() {//Call a function when the state changes.
+		if(http.readyState == 4 && http.status == 200) {
+			console.log("Server response: "+http.responseText);
+		}
+	}
+	http.send(params);
 });
 
 
