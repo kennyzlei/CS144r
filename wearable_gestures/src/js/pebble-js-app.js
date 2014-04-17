ba95e6dd34b18795babec349b70f4ca70d410f5b
Benjy Levin <benjylevin@college.harvard.edu>
2014-04-16 23:57:25 -0400

    hosted the html file on our github so now accessible through the settings on the app

--- wearable_gestures/src/js/pebble-js-app.js
+++ wearable_gestures/src/js/pebble-js-app.js
@@ -138,7 +138,7 @@ function max_peak(v){
 
 Pebble.addEventListener("showConfiguration", function() {
   console.log("showing configuration");
-  Pebble.openURL('http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-js/configurable.html');
+  Pebble.openURL('http://kennyzlei.github.io/CS144r');//'http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-js/configurable.html');
 });
 
 Pebble.addEventListener("webviewclosed",
