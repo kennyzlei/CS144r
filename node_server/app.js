137b0f2f1bd5c119af0550b3fb461f49fc37e7ec
Kenny Lei <klei@jpl.nasa.gov>
2014-04-27 20:35:08 -0400

    console logging shiftcompare

--- node_server/app.js
+++ node_server/app.js
@@ -3,6 +3,80 @@ var qs = require('querystring');
 
 var shake = new Array();
 
+function shiftCompare(a,b){
+    var dotproductvals = new Array();
+    var lim;
+    var siga,invsiga;
+    var sigb;
+    //if (a.length <= b.length){
+        siga = normalize(a);
+        siga.reverse();
+    for (i=0; i<b.length;i++)
+        {
+            siga.push(0);
+        }   
+        siga.reverse();
+        for (i=0; i<b.length;i++)
+        {
+            siga.push(0);
+        }
+
+        //invsiga.reverse();
+        sigb = normalize(b);
+        lim = siga.length;
+        for (var i = 0; i < lim; i++) {
+            var shiftsig = siga.slice(lim-i-1);
+            dotproductvals.push(dotproduct(shiftsig,sigb));
+        };
+        var result = returnMatchedVectors(siga,sigb,dotproductvals);
+        return result;
+    };
+
+function dotproduct(a,b) {
+    var n = 0, lim = Math.min(a.length,b.length);
+    for (var i = 0; i < lim; i++) n += a[i] * b[i];
+    return n;
+ }
+
+function normalize(a){
+    var length = Math.sqrt(dotproduct(a,a));
+    var result = a.map(function(d){return d/length});
+    return result;
+}
+
+function magnitude(a){
+    var length = Math.sqrt(dotproduct(a,a));
+    return length;  
+}
+
+function returnMatchedVectors(a,b,dotprodvals){
+    var max = dotprodvals[0];
+    var maxIndex = 0;
+
+for (var i = 1; i < dotprodvals.length; i++) {
+    if (dotprodvals[i] > max) {
+        maxIndex = i;
+        max = dotprodvals[i];
+    }
+}
+a.reverse();
+
+var warpeda = a.slice(maxIndex - b.length+1,maxIndex+1).reverse();
+var warpedb = b;
+
+var result = cosineSimilarity(warpeda,warpedb);
+return result;
+}
+
+function cosineSimilarity(a,b){
+var dotprod = dotproduct(a,b);
+var maga = magnitude(a);
+var magb = magnitude(b);
+var multmag = maga*magb;
+return ((dotprod/multmag)*100);
+}
+
+
 function Stack(lat, longit)
 {
     this.latitude = lat
@@ -61,6 +135,7 @@ http.createServer(function (request, response) {
                 response.write(entry[2]+"\n");
                 response.write(entry[3]+"\n");
                 response.end(entry[4]);
+                console.log(shiftCompare(entry[5], formData.acceleration))
             }
             else {
                 response.end("No match");
@@ -73,7 +148,7 @@ http.createServer(function (request, response) {
             set = false;
             while (i < shake.length) {
                 if (Math.sqrt(Math.pow(shake[i].latitude - formData.latitude, 2) + Math.pow(shake[i].longitude - formData.longitude, 2)) < 1000){
-                    shake[i].push([formData.account, formData.time, formData.name, formData.phonenumber, formData.email]);
+                    shake[i].push([formData.account, formData.time, formData.name, formData.phonenumber, formData.email, formData.acceleration]);
                     set = true;
                     break;
                 }
@@ -81,7 +156,7 @@ http.createServer(function (request, response) {
             }
             if (set == false) {
                 new_shake = new Stack(formData.latitude, formData.longitude);
-                new_shake.push([formData.account, formData.time, formData.name, formData.phonenumber, formData.email]);
+                new_shake.push([formData.account, formData.time, formData.name, formData.phonenumber, formData.email, formData.acceleration]);
                 shake.push(new_shake);
             }
             response.writeHead(200, {'Content-Type': 'text/plain'});
